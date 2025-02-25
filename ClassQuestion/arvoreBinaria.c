b4ad89cd503d82b3eef7d0160105c71235e1d520
LuanAlmeidaValenca <luan.valenca@hotmail.com>
2025-02-25 23:15:08 +0000

    testeteste

--- ClassQuestion/arvoreBinaria.c
+++ ClassQuestion/arvoreBinaria.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
 
+typedef struct tipo_no{
+    int dado;
+    tipo_no *pai;
+    tipo_no *filhoESQ;
+    tipo_no *filhoDIR; 
+}No;
+
+typedef struct Arv{
+    int tamanho;
+    No *primeiro;
+} Arvore;
+
+void criarArv(Arvore arvore){
+    arvore.tamanho = 0;
+    arvore.primeiro = NULL;
+}
+
+void inserirNo(Arvore *arvore, int dado){
+    No *novo = (No *) malloc(sizeof(No));
+    if(!novo){
+        printf("\nErro na alocação de memoria");
+        return;
+    }
+    novo->dado = dado;
+    No aux = arvore->primeiro;
+
+    if(arvore->primeiro == NULL){
+        arvore->primeiro = novo;
+        novo->pai = arvore;
+    }else {
+        if(aux->dado > dado){
+            aux->filhoESQ; 
+        }
+    }   
+
+}
+
+int main(){
+
+
+
+    return 0;
+}
